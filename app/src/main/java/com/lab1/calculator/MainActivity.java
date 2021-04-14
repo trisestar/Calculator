aded7593ff564731079675c85532aaf72b618b86
Artyom <a21plusartyom@gmail.com>
2021-04-14 22:02:38 +0300

    Bugfixes

--- app/src/main/java/com/lab1/calculator/MainActivity.java
+++ app/src/main/java/com/lab1/calculator/MainActivity.java
@@ -12,6 +12,8 @@ import org.mariuszgromada.math.mxparser.*;
 public class MainActivity extends AppCompatActivity {
 
     private EditText display;
+    private boolean isFirstPoint = true;
+    private int parenthesesNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,146 +25,174 @@ public class MainActivity extends AppCompatActivity {
         display.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (getString(R.string.display).equals(display.getText().toString())){
+                if (getString(R.string.display).equals(display.getText().toString())) {
                     display.setText("");
                 }
             }
         });
     }
 
-    private void updateText (String strToAdd){
+    private void updateText(String strToAdd) {
         String oldStr = display.getText().toString();
         int cursorPos = display.getSelectionStart();
-        String leftStr = oldStr.substring(0,cursorPos);
+        String leftStr = oldStr.substring(0, cursorPos);
         String rightStr = oldStr.substring(cursorPos);
-        if(getString(R.string.display).equals(display.getText().toString())){
+        if (getString(R.string.display).equals(display.getText().toString())) {
             display.setText(strToAdd);
-            display.setSelection(cursorPos+1);
-        }
-        else{
-            display.setText(String.format("%s%s%s", leftStr, strToAdd,rightStr));
+            display.setSelection(cursorPos + 1);
+        } else {
+            display.setText(String.format("%s%s%s", leftStr, strToAdd, rightStr));
             display.setSelection(cursorPos + 1);
         }
 
     }
 
-    public void zeroBTN(View view){
+    private void secondSymbolCheck(String strToAdd) {
+        int cursorPos = display.getSelectionStart();
+        String oldStr = String.valueOf(display.getText());
+        char[] oldChar = oldStr.toCharArray();
+        if (cursorPos != 0) {
+            if (oldChar[cursorPos - 1] == '0' || oldChar[cursorPos - 1] == '1' || oldChar[cursorPos - 1] == '2' || oldChar[cursorPos - 1] == '3' ||
+                    oldChar[cursorPos - 1] == '4' || oldChar[cursorPos - 1] == '5' || oldChar[cursorPos - 1] == '6' || oldChar[cursorPos - 1] == '7' ||
+                    oldChar[cursorPos - 1] == '8' || oldChar[cursorPos - 1] == '9' || oldChar[cursorPos - 1] == ')'){
+                updateText(strToAdd);
+                isFirstPoint = true;
+            }
+        }
+    }
+
+    public void zeroBTN(View view) {
         updateText("0");
 
     }
 
-    public void oneBTN(View view){
+    public void oneBTN(View view) {
         updateText("1");
 
     }
-    public void twoBTN(View view){
+
+    public void twoBTN(View view) {
 
         updateText("2");
     }
-    public void threeBTN(View view){
+
+    public void threeBTN(View view) {
         updateText("3");
 
     }
-    public void fourBTN(View view){
+
+    public void fourBTN(View view) {
         updateText("4");
 
     }
-    public void fiveBTN(View view){
+
+    public void fiveBTN(View view) {
         updateText("5");
 
     }
-    public void sixBTN(View view){
+
+    public void sixBTN(View view) {
 
         updateText("6");
     }
-    public void sevenBTN(View view){
+
+    public void sevenBTN(View view) {
         updateText("7");
 
     }
-    public void eightBTN(View view){
+
+    public void eightBTN(View view) {
         updateText("8");
 
     }
-    public void nineBTN(View view){
+
+    public void nineBTN(View view) {
 
         updateText("9");
     }
 
 
-    public void clearBTN(View view){
+    public void clearBTN(View view) {
 
         display.setText("");
     }
-    public void exponentBTN(View view){
-        updateText("^");
+
+    public void exponentBTN(View view) {
+        secondSymbolCheck("^");
 
     }
-    public void parenthesesBTN(View view){
-        int cursorPos = display.getSelectionStart();
-        int openPar = 0;
-        int closedPar = 0;
-        int textLen = display.getText().length();
-        for (int i=0; i<cursorPos; i++){
-            if (display.getText().toString().substring(i,i+1).equals("(")    ){
-                openPar+=1;
-            }
-            if (display.getText().toString().substring(i,i+1).equals(")")    ){
-                closedPar+=1;
-            }
 
-        }
-        if (openPar==closedPar || display.getText().toString().substring(textLen-1,textLen).equals("(")){
+    public void parenthesesBTN(View view) {
+        int cursorPos = display.getSelectionStart();
+        String oldStr = String.valueOf(display.getText());
+        char[] oldChar = oldStr.toCharArray();
+        if (cursorPos != 0) {
+            if (oldChar[cursorPos - 1] == '0' || oldChar[cursorPos - 1] == '1' || oldChar[cursorPos - 1] == '2' || oldChar[cursorPos - 1] == '3' ||
+                    oldChar[cursorPos - 1] == '4' || oldChar[cursorPos - 1] == '5' || oldChar[cursorPos - 1] == '6' || oldChar[cursorPos - 1] == '7' ||
+                    oldChar[cursorPos - 1] == '8' || oldChar[cursorPos - 1] == '9' || oldChar[cursorPos - 1] == ')'){
+                if (parenthesesNum>0) {
+                    parenthesesNum--;
+                    updateText(")");
+                }
+            }
+            if (oldChar[cursorPos - 1] == '+' || oldChar[cursorPos - 1] == '-' || oldChar[cursorPos - 1] == '÷' || oldChar[cursorPos - 1] == '×' ||
+                    oldChar[cursorPos - 1] == '^' || oldChar[cursorPos - 1] == '(' ){
+                parenthesesNum++;
+                updateText("(");
+            }
+        } else {
+            parenthesesNum++;
             updateText("(");
-
         }
-        else if (closedPar < openPar && !display.getText().toString().substring(textLen-1,textLen).equals("(")){
-            updateText(")");
+    }
+
+
+    public void divideBTN(View view) {
+        secondSymbolCheck("÷");
+    }
+
+    public void multiplyBTN(View view) {
+        secondSymbolCheck("×");
+    }
+
+    public void addBTN(View view) {
+        secondSymbolCheck("+");
+    }
+
+    public void subtractBTN(View view) {
+        secondSymbolCheck("-");
+    }
+
+    public void plusMinusBTN(View view) {
+        secondSymbolCheck("+/-");
+    }
+
+    public void pointBTN(View view) {
+        if (isFirstPoint == true) {
+            secondSymbolCheck(".");
+            isFirstPoint = false;
         }
-        display.setSelection(cursorPos+1);
     }
-    public void divideBTN(View view){
 
-        updateText("÷");
-    }
-    public void multiplyBTN(View view){
-        updateText("×");
-
-    }
-    public void addBTN(View view){
-
-        updateText("+");
-    }
-    public void subtractBTN(View view){
-        updateText("-");
-
-    }
-    public void plusMinusBTN(View view){
-        updateText("+/-");
-
-    }
-    public void pointBTN(View view){
-        int cursorPos = display.getSelectionStart();
-        updateText(".");
-
-    }
-    public void equalsBTN(View view){
+    public void equalsBTN(View view) {
         String userExp = display.getText().toString();
-        userExp = userExp.replaceAll("÷","/");
-        userExp = userExp.replaceAll("×","*");
+        userExp = userExp.replaceAll("÷", "/");
+        userExp = userExp.replaceAll("×", "*");
         Expression exp = new Expression(userExp);
         String result = String.valueOf(exp.calculate());
         display.setText(result);
         display.setSelection(result.length());
 
     }
-    public void backspaceBTN(View view){
+
+    public void backspaceBTN(View view) {
         int cursorPos = display.getSelectionStart();
         int textLen = display.getText().length();
-        if(cursorPos != 0 && textLen !=0){
+        if (cursorPos != 0 && textLen != 0) {
             SpannableStringBuilder selection = (SpannableStringBuilder) display.getText();
-            selection.replace(cursorPos-1,cursorPos,"");
+            selection.replace(cursorPos - 1, cursorPos, "");
             display.setText(selection);
-            display.setSelection(cursorPos-1);
+            display.setSelection(cursorPos - 1);
         }
 
     }
